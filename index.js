88e308961f57315fa284994ebbc3fb2f242a5b0a
Amruke1 <neziriamra29@gmail.com>
2025-01-21 22:23:06 +0100

    .

---
+++ index.js
@@ -0,0 +1,5 @@
+// Automatically show the modal when the page loads
+window.onload = function() {
+    var myModal = new bootstrap.Modal(document.getElementById('AN-myModal'));
+    myModal.show();
+}
